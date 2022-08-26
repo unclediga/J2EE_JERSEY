0dca6155c8a47bd54a5a521a8de6fa43127e7d78
unclediga <zowie_i@mail.ru>
2022-08-26 16:50:22 +0300

    test JSON-B vs JAXB

--- jersey-saburov/src/main/java/ru/unclediga/examples/jersey/domain/MyData.java
+++ jersey-saburov/src/main/java/ru/unclediga/examples/jersey/domain/MyData.java
@@ -3,25 +3,53 @@ package ru.unclediga.examples.jersey.domain;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlElement;
+import javax.json.bind.annotation.JsonbProperty;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-public class MyData {
+public class MyData { 
+    @JsonbProperty("s1")
     private String s;
+    @XmlElement(name = "s2")
+    private String ss;
+    private String sss;
 
     public MyData() {
     }
 
     public MyData(String s) {
         this.s = s;
+        this.ss = s;        
+	this.sss = s;	
     }
 
-    @Override
-    public String toString() {
-        return "MyData{'" + s + "'}";
+    public String getS() {
+        return s;
+    }
+    public void setS(String s) {
+        this.s = s;
+    }
+    public String getSs() {
+        return ss;
+    }
+    public void setSs(String s) {
+        this.ss = s;
+    }    
+
+    @JsonbProperty("s3")
+    public String getSss() {
+        return sss;
+    }
+    public void setSss(String s) {
+        this.sss = s;
     }
 
     public String toJson() {
         return "{\"s\" : \"" +  s + "\"}";
+    }
+    @Override
+    public String toString() {
+        return "MyData{'" + s + "'}";
     }
 }
---
+++ runArch.bat
@@ -0,0 +1,4 @@
+mvn archetype:generate -DarchetypeArtifactId=jersey-quickstart-grizzly2 ^
+-DarchetypeGroupId=org.glassfish.jersey.archetypes -DinteractiveMode=false    ^
+-DgroupId=ru.unclediga  -DartifactId=jersey-simple -Dpackage=ru.unclediga.jersey ^
+-DarchetypeVersion=2.35
